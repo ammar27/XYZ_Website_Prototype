2cbb1e66b617f93613b40d576303d3b10752cbbf
unknown <abag505@EN-284083.UoA.auckland.ac.nz>
2016-05-17 18:15:37 +1200

    Improved door locks screen

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-window.onload = function() {
-  $(document).ready(function() {
-    // Function for replacing dashboard div with door locks div
-    $("#door-tile").click(function () {
-      alert("trying to redirect");
-      //$((document.getElementById("dashboard-screen")).replaceWith(document.getElementById("door-lock-screen"));
-      $("#dashboard-screen").load("doorLockScreen.html");
-    });
+$(document).ready(function() {
+  // Function for replacing dashboard div with door locks div
+  $("#door-tile").click(function () {
+    $("#dashboard-screen").replaceWith(document.getElementById("lock-screen"));
+    document.getElementById("lock-screen").style.display="block";
+    //$("#dashboard-screen").load("doorLockScreen.html");
   });
-
-
-
-};
+});
