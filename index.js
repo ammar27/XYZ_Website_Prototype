b5c785b0006fdad0fc0cd774fbb2a15d4bdba1cf
Ammar Bagasrawala <ammarb27@gmail.com>
2016-05-18 23:15:37 +1200

    Added motion sensor screen

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
   // Function for replacing dashboard div with door locks div
   $("#door-tile").click(function () {
-    var dashBoardScreen = $("#dashBoardScreen").clone(true);
     $("#dashboard-screen").replaceWith(document.getElementById("lock-screen"));
     document.getElementById("lock-screen").style.display="block";
     //$("#dashboard-screen").load("doorLockScreen.html");
   });
-});
 
-/* Navbar collapse function source:http://www.w3schools.com/howto/howto_js_navbar-top.asp */
-function navbarCollapse() {
-    document.getElementsByClassName("navbar-top")[0].classList.toggle("responsive");
-}
+  // Function for replacing dashboard div with door locks div
+  $("#motion-sensors-tile").click(function () {
+    $("#dashboard-screen").replaceWith(document.getElementById("motion-sensors-screen"));
+    document.getElementById("motion-sensors-screen").style.display="block";
+    //$("#dashboard-screen").load("doorLockScreen.html");
+  });
+
+});
