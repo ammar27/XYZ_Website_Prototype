1d10799503fffe1cc30544ffc3fcb4bf92bcee32
Ammar Bagasrawala <ammarb27@gmail.com>
2016-05-18 15:47:20 +1200

    Fixed navbar problems

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ $(document).ready(function() {
     //$("#dashboard-screen").load("doorLockScreen.html");
   });
 });
+
+/* Navbar collapse function source:http://www.w3schools.com/howto/howto_js_navbar-top.asp */
+function navbarCollapse() {
+    document.getElementsByClassName("navbar-top")[0].classList.toggle("responsive");
+}
