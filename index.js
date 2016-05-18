f6c97cbb9757173c47eb40e37b0bd04b60a5254b
Ammar Bagasrawala <ammarb27@gmail.com>
2016-05-18 22:08:30 +1200

    Finished layout of door locks screen

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   // Function for replacing dashboard div with door locks div
   $("#door-tile").click(function () {
+    var dashBoardScreen = $("#dashBoardScreen").clone(true);
     $("#dashboard-screen").replaceWith(document.getElementById("lock-screen"));
     document.getElementById("lock-screen").style.display="block";
     //$("#dashboard-screen").load("doorLockScreen.html");
