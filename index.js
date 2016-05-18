c745fe1199a4389c15cdd72f0ad8105eb0e736d4
Ammar Bagasrawala <ammarb27@gmail.com>
2016-05-18 23:50:42 +1200

    Added surveillance screen

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ $(document).ready(function() {
     //$("#dashboard-screen").load("doorLockScreen.html");
   });
 
-  // Function for replacing dashboard div with door locks div
+  // Function for replacing dashboard div with motion sensor div
   $("#motion-sensors-tile").click(function () {
     $("#dashboard-screen").replaceWith(document.getElementById("motion-sensors-screen"));
     document.getElementById("motion-sensors-screen").style.display="block";
     //$("#dashboard-screen").load("doorLockScreen.html");
   });
 
+  // Function for replacing dashboard div with surveillance div
+  $("#surveillance-tile").click(function () {
+    $("#dashboard-screen").replaceWith(document.getElementById("surveillance-screen"));
+    document.getElementById("surveillance-screen").style.display="block";
+    //$("#dashboard-screen").load("doorLockScreen.html");
+  });
 });
