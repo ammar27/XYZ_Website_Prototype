b69990225231e72b5f3bd983539e39431fbd3d8f
Ammar Bagasrawala <ammarb27@gmail.com>
2016-05-20 14:22:50 +1200

    Changed colour and text weight for lock, unlock, on and off labels

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   // Setting up colours for all lock/unlock and on/off text in the screens
   var text = document.getElementById("motion-living-text");
   if (text.innerHTML === "ON") {
-    text.style.color = "blue";
+    //text.style.color = "";
   }
 
 
@@ -36,63 +36,75 @@ $(document).ready(function() {
   // Obtains properties for door locks and changes state based on previous state
   $("#front-door-lock-slider").click(function () {
     var lock = document.getElementById("front-door-lock");
-    if (lock.innerHTML === "LOCKED") { lock.innerHTML = "UNLOCKED"; } else { lock.innerHTML = "LOCKED"; }
+    if (lock.innerHTML === "LOCKED") { lock.innerHTML = "UNLOCKED"; lock.style.color = "#e7604a";
+    } else { lock.innerHTML = "LOCKED"; lock.style.color = "#2b78c2"; }
   });
 
   $("#back-door-lock-slider").click(function () {
     var lock = document.getElementById("back-door-lock");
-    if (lock.innerHTML === "LOCKED") { lock.innerHTML = "UNLOCKED"; } else { lock.innerHTML = "LOCKED"; }
+    if (lock.innerHTML === "LOCKED") { lock.innerHTML = "UNLOCKED"; lock.style.color = "#e7604a";
+    } else { lock.innerHTML = "LOCKED"; lock.style.color = "#2b78c2"; }
   });
 
   $("#garage-door-lock-slider").click(function () {
     var lock = document.getElementById("garage-door-lock");
-    if (lock.innerHTML === "LOCKED") { lock.innerHTML = "UNLOCKED"; } else { lock.innerHTML = "LOCKED"; }
+    if (lock.innerHTML === "LOCKED") { lock.innerHTML = "UNLOCKED"; lock.style.color = "#e7604a";
+    } else { lock.innerHTML = "LOCKED"; lock.style.color = "#2b78c2"; }
   });
 
   $("#deck-door-lock-slider").click(function () {
     var lock = document.getElementById("deck-door-lock");
-    if (lock.innerHTML === "LOCKED") { lock.innerHTML = "UNLOCKED"; } else { lock.innerHTML = "LOCKED"; }
+    if (lock.innerHTML === "LOCKED") { lock.innerHTML = "UNLOCKED"; lock.style.color = "#e7604a";
+    } else { lock.innerHTML = "LOCKED"; lock.style.color = "#2b78c2"; }
   });
 
   //Obtains properties for motion sensor switches and changes state based on previous state
   $("#motion-living-slider").click(function () {
     var switchText = document.getElementById("motion-living-text");
-    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; } else { switchText.innerHTML = "ON"; }
+    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; switchText.style.color = "#e7604a";
+    } else { switchText.innerHTML = "ON"; switchText.style.color = "#2b78c2"; }
   });
 
   $("#motion-bed-slider").click(function () {
     var switchText = document.getElementById("motion-bed-text");
-    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; } else { switchText.innerHTML = "ON"; }
+    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; switchText.style.color = "#e7604a";
+    } else { switchText.innerHTML = "ON"; switchText.style.color = "#2b78c2"; }
   });
 
   $("#motion-entrance-slider").click(function () {
     var switchText = document.getElementById("motion-entrance-text");
-    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; } else { switchText.innerHTML = "ON"; }
+    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; switchText.style.color = "#e7604a";
+    } else { switchText.innerHTML = "ON"; switchText.style.color = "#2b78c2"; }
   });
 
   $("#motion-kitchen-slider").click(function () {
     var switchText = document.getElementById("motion-kitchen-text");
-    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; } else { switchText.innerHTML = "ON"; }
+    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; switchText.style.color = "#e7604a";
+    } else { switchText.innerHTML = "ON"; switchText.style.color = "#2b78c2"; }
   });
 
   //Obtains properties for surveillance switches and changes state based on previous state
   $("#surv-living-slider").click(function () {
     var switchText = document.getElementById("surv-living-text");
-    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; } else { switchText.innerHTML = "ON"; }
+    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; switchText.style.color = "#e7604a";
+    } else { switchText.innerHTML = "ON"; switchText.style.color = "#2b78c2"; }
   });
 
   $("#surv-bed-slider").click(function () {
     var switchText = document.getElementById("surv-bed-text");
-    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; } else { switchText.innerHTML = "ON"; }
+    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; switchText.style.color = "#e7604a";
+    } else { switchText.innerHTML = "ON"; switchText.style.color = "#2b78c2"; }
   });
 
   $("#surv-entrance-slider").click(function () {
     var switchText = document.getElementById("surv-entrance-text");
-    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; } else { switchText.innerHTML = "ON"; }
+    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; switchText.style.color = "#e7604a";
+    } else { switchText.innerHTML = "ON"; switchText.style.color = "#2b78c2"; }
   });
 
   $("#surv-kitchen-slider").click(function () {
     var switchText = document.getElementById("surv-kitchen-text");
-    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; } else { switchText.innerHTML = "ON"; }
+    if (switchText.innerHTML === "ON") { switchText.innerHTML = "OFF"; switchText.style.color = "#e7604a";
+    } else { switchText.innerHTML = "ON"; switchText.style.color = "#2b78c2"; }
   });
 });
